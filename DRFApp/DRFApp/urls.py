41e5e42abcbcde5958c2bab14adf8bb9e47c8b27
devankit01 <devankitgupta01@gmail.com>
2022-01-23 20:54:48 +0530

    �

--- DRFApp/DRFApp/urls.py
+++ DRFApp/DRFApp/urls.py
@@ -5,11 +5,18 @@ from django.urls import path,include
 from rest_framework.routers import DefaultRouter
 from DRFShort.views import *
 
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
 router.register('courses' , CourseViewSet,  basename = 'course')
 
 from DRFNested.views import *
+
+# JWT Token
+from rest_framework_simplejwt.views import (
+TokenObtainPairView,
+TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -19,9 +26,15 @@ urlpatterns = [
     # Nested Route
     path('ns/api/',InstructorListView.as_view()),
     path('ns/api/<pk>/',InstructorDetailView.as_view(),name="instructor-detail"),
-    path('courses/api/',CourseListView.as_view()),
-    path('courses/api/<pk>/',CourseDetailView.as_view(),name="course-detail"),
+    path('ns/courses/api/',CourseListView.as_view()),
+    path('ns/courses/api/<pk>/',CourseDetailView.as_view(),name="course-detail"),
+    path('ns/auth/login/',obtain_auth_token,name="create-token"),
+
+    # JWT Token
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
 
 ]
 
+# Note : Use Bearer_JWTToken for sending auth Request
--- DRFApp/DRFNested/views.py
+++ DRFApp/DRFNested/views.py
@@ -7,9 +7,28 @@ from .serializers import *
 from .models import *
 
 # Permissions
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.authentication import BasicAuthentication
+# from rest_framework.permissions import IsAuthenticated, IsAdminUser, BasePermission
+# from rest_framework.authentication import BasicAuthentication, TokenAuthentication
 
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated, BasePermission
+
+from rest_framework.authtoken.models import Token
+
+
+# Admin Permission
+class WriteByAdmin(BasePermission):
+    def has_permission(self,request, view):
+        print(request.user)
+
+        user = request.user
+        if request.method == "GET":
+            return True
+        else:
+            if user.is_superuser:
+                return True
+            else:
+                False
 
 class InstructorListView(generics.ListCreateAPIView):
     queryset =  Instructor.objects.all()
@@ -20,8 +39,8 @@ class InstructorDetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = InstructorSerializer
 
 class CourseListView(generics.ListCreateAPIView):
-    authentication_classes = [BasicAuthentication] # Auth
-    permission_classes = [IsAuthenticated] # Add Permission
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated,WriteByAdmin]
     queryset =  Course.objects.all()
     serializer_class = CourseSerializer
 
